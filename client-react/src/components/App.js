9110c229bd5afdca4f92ac785e122b7fa4203e84
L3n1ad <danieltoth0910@gmail.com>
2020-06-17 12:39:39 +0100

    add react-routed-rom and routes with link tags

--- client-react/src/components/App.js
+++ client-react/src/components/App.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const PageOne = () => {
+  return (
+    <div>
+      {" "}
+      Page 1
+      {/* WRONG APPROACH - because it would refresh the page meaning all of states and props would be all gone and set back to that initial state */}
+      {/* <a href="/pagetwo"> Get me to page two</a> */}
+      <Link to="/pagetwo"> Get me to page two</Link>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      {" "}
+      Page 2 <Link to="/"> Get me to page </Link>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div> APP </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          {/* exact property makes sure that home page component will show only exactly on the home page url */}
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
