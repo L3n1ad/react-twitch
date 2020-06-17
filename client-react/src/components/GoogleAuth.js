a8d0860ba8ac87334effaffb03c0baf4c6412db8
L3n1ad <danieltoth0910@gmail.com>
2020-06-17 13:55:02 +0100

    create GoogleAuth component with basic config

---
+++ client-react/src/components/GoogleAuth.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+
+const GoogleAuth = () => {
+  // window. important to tell react that it is available on window scope
+  useEffect(() => {
+    //   add call back to make sure it will be called only once the data return from google
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId: process.env.REACT_APP_GOOGLE_AUTH_KEY,
+        scpoe: "email",
+      });
+    });
+  }, []);
+  return <div>GOOGLE AUTH</div>;
+};
+
+export default GoogleAuth;
