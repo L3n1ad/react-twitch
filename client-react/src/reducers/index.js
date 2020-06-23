9b4b57ee5c75b864a42afb2487981438b2101d9a
L3n1ad <danieltoth0910@gmail.com>
2020-06-23 13:16:47 +0100

    hook up the app with redux-form library

--- client-react/src/reducers/index.js
+++ client-react/src/reducers/index.js
@@ -1,6 +1,11 @@
-import { combinedReducers, combineReducers } from "redux";
+import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+// we can rename the imported feature straight away to make more sense
+import { reducer as formReducer } from "redux-form";
 
+// reducer needs to be wired up from redux-from
+// it needs to be signed under a name form
 export default combineReducers({
   auth: authReducer,
+  form: formReducer,
 });
