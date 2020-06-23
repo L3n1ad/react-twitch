37c2cfad85d04a0aff05415dbdadfed57913433a
L3n1ad <danieltoth0910@gmail.com>
2020-06-23 13:28:24 +0100

    add form event handeler with redux-form handleSubmit function

--- client-react/src/components/streams-routes/StreamCreate.js
+++ client-react/src/components/streams-routes/StreamCreate.js
@@ -22,10 +22,19 @@ class StreamCreate extends React.Component {
     // );
   }
 
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
   render() {
-    console.log(this.props);
+    // to see what we get from redux-form
+    // console.log(this.props);
     return (
-      <form className="ui form">
+      // we need to call redux-form handleSubmit function and pass our own formSubmit function as an argument
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
         {/* some proprs are always required such as name,  */}
         {/* the field element has no idea how to render anything on the screen its need a component prop to render a component or call a function which returns a element which can be rendered on the screen */}
         {/* Field will automatically pass unknown properties to the function so we will have access to it */}
@@ -35,6 +44,7 @@ class StreamCreate extends React.Component {
           component={this.renderInput}
           label="Enter Description"
         />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
