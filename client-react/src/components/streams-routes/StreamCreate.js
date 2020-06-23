8c7e4e26a17e3c38c48ac5f24cd770ec9a2dcb0a
L3n1ad <danieltoth0910@gmail.com>
2020-06-23 13:17:58 +0100

    add inputs and hook them up with redux-form

--- client-react/src/components/streams-routes/StreamCreate.js
+++ client-react/src/components/streams-routes/StreamCreate.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreameCreate</div>;
-};
+class StreamCreate extends React.Component {
+  renderInput({ input }) {
+    // MODERN SYNTAX
+    return <input {...input} />;
+    // OLDER SyNTAX
+    // renderInput(formProps)
+    // console.log(formProps);
+    // return (
+    //   <input
+    //     onChange={formProps.input.onChange}
+    //     value={formProps.input.value}
+    //   />
+    // );
+  }
 
-export default StreamCreate;
+  render() {
+    console.log(this.props);
+    return (
+      <form>
+        {/* some proprs are always required such as name,  */}
+        {/* the field element has no idea how to render anything on the screen its need a component prop to render a component or call a function which returns a element which can be rendered on the screen */}
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+
+// it is essentialy the same thing as connect with redux
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
