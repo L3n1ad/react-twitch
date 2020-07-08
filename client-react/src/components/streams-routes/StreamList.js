982402d468261a1cbb1d1bca334104353341dbb8
L3n1ad <danieltoth0910@gmail.com>
2020-07-08 17:43:46 +0100

    add mapStateToProps and some ui elements to render the props on the screen

--- client-react/src/components/streams-routes/StreamList.js
+++ client-react/src/components/streams-routes/StreamList.js
@@ -7,9 +7,34 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderList() {
+    return this.props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>StreamList</div>;
+    console.log(this.props.streams);
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+      </div>
+    );
   }
 }
 
-export default connect(null, { fetchStreams })(StreamList);
+const mapStateToProps = (state) => {
+  // we format the object into an array cause it is easier to handle in our component
+  return { streams: Object.values(state.streams) };
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
