205815be8b9dc450195146f9cd7110d1df06948a
L3n1ad <danieltoth0910@gmail.com>
2020-07-08 17:33:23 +0100

    import streamReducer to combineReducers

--- client-react/src/reducers/index.js
+++ client-react/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 // we can rename the imported feature straight away to make more sense
 import { reducer as formReducer } from "redux-form";
+import streamReducer from "./streamReducer";
 
 // reducer needs to be wired up from redux-from
 // it needs to be signed under a name form
 export default combineReducers({
   auth: authReducer,
   form: formReducer,
+  streams: streamReducer,
 });
