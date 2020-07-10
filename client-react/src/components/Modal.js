8af87e7c34c32a3be5949359b7b03a0e03bf5001
L3n1ad <danieltoth0910@gmail.com>
2020-07-10 12:25:01 +0100

    refactor Modal component to make it reusable

--- client-react/src/components/Modal.js
+++ client-react/src/components/Modal.js
@@ -9,22 +9,14 @@ const Modal = (props) => {
     //   in order to make sure the history.push works only out of the popup we need to
     // define onClick on the child element to it won't boil up to the parent element to find
     //  an onClick USE e.stopPropagation()
-    <div
-      onClick={() => history.push("/")}
-      className="ui dimmer modals visible active"
-    >
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
       <div
         onClick={(e) => e.stopPropagation()}
         className="ui standard modal visible active"
       >
-        <div className="header">Delete Stream</div>
-        <div className="conent">
-          Are you sure you want to delet this stream?
-        </div>
-        <div className="actions">
-          <button className="ui button">Cancel</button>
-          <button className="ui primary button">Delete</button>
-        </div>
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector("#modal")
