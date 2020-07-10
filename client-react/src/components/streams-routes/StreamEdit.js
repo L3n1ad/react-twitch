5207c0d9d39255fe2c780cc5aa0671924064d700
L3n1ad <danieltoth0910@gmail.com>
2020-07-10 11:20:51 +0100

    refactor StreamEdit to use StreamForm

--- client-react/src/components/streams-routes/StreamEdit.js
+++ client-react/src/components/streams-routes/StreamEdit.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { fetchStream, editStream } from "../../actions/index";
 import StreamForm from "./StreamForm";
+import _ from "lodash";
 
 class StreamEdit extends React.Component {
   componentDidMount() {
@@ -9,7 +10,7 @@ class StreamEdit extends React.Component {
   }
 
   onSubmit = (formValues) => {
-    console.log(formValues);
+    this.props.editStream(this.props.stream.id, formValues);
   };
 
   render() {
@@ -22,7 +23,12 @@ class StreamEdit extends React.Component {
         {/* initialValues is a special thing for redux Form which will use the pass down values to fill up the fields in the form, naming is important it has to be the same as the naming in the form */}
         <StreamForm
           // initialValues={{ title: "EDIT ME", description: "CHANGE ME TOO" }}
-          initialValues={this.props.stream}
+          // initialValues={{
+          //   title: this.props.stream.title,
+          //   description: this.props.stream.description,
+          // }}
+          // SOLUTION BY LODASH
+          initialValues={_.pick(this.props.stream, "title", "description")}
           onSubmit={this.onSubmit}
         />
       </div>
