44d5d66b274bace4fac85f62c4827cb9264573fa
L3n1ad <danieltoth0910@gmail.com>
2020-07-10 10:47:06 +0100

    clean up/refactor StreamCreate to use StreamForm

--- client-react/src/components/streams-routes/StreamCreate.js
+++ client-react/src/components/streams-routes/StreamCreate.js
@@ -1,90 +1,21 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 import { createStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamCreate extends React.Component {
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  }
-
-  renderInput = ({ input, label, meta }) => {
-    // MODERN SYNTAX
-    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
-
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
-      </div>
-    );
-
-    // OLDER SyNTAX
-    // renderInput(formProps)
-    // console.log(formProps);
-    // return (
-    //   <input
-    //     onChange={formProps.input.onChange}
-    //     value={formProps.input.value}
-    //   />
-    // );
-  };
-
   onSubmit = (formValues) => {
     this.props.createStream(formValues);
   };
 
   render() {
-    // to see what we get from redux-form
-    // console.log(this.props);
     return (
-      // we need to call redux-form handleSubmit function and pass our own formSubmit function as an argument
-      // error is needed in the class name to display the error message otherwise all error messages is set to display:none
-      <form
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form error"
-      >
-        {/* some proprs are always required such as name,  */}
-        {/* the field element has no idea how to render anything on the screen its need a component prop to render a component or call a function which returns a element which can be rendered on the screen */}
-        {/* Field will automatically pass unknown properties to the function so we will have access to it */}
-        <Field name="title" component={this.renderInput} label="Enter Title" />
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="Enter Description"
-        />
-        <button className="ui button primary">Submit</button>
-      </form>
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
-// If there is no error messages it will return an empty object which will tell redux-form that the form is validated and it can be submitted
-// this function is called every single time the user interacts with the form
-const validate = (formValues) => {
-  const errors = {};
-  if (!formValues.title) {
-    errors.title = "You must enter a title";
-  }
-  if (!formValues.description) {
-    errors.description = "You must enter a description";
-  }
-
-  return errors;
-};
-
-// it is essentialy the same thing as connect with redux
-// validate is a key word just as form they need to be called like that
-const formWrapped = reduxForm({
-  form: "streamCreate",
-  validate: validate,
-})(StreamCreate);
-
-export default connect(null, { createStream })(formWrapped);
+export default connect(null, { createStream })(StreamCreate);
