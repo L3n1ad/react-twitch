64a85c188d3d61de0a6e36cd7375180a4209424e
L3n1ad <danieltoth0910@gmail.com>
2020-07-10 13:20:23 +0100

    add react Switch to make sure we render only the component matches the path

--- client-react/src/components/App.js
+++ client-react/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import StreamCreate from "./streams-routes/StreamCreate";
 import StreamDelete from "./streams-routes/StreamDelete";
 import StreamEdit from "./streams-routes/StreamEdit";
@@ -10,17 +10,18 @@ import history from "../history";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <Router history={history}>
-        <Header />
-        <div className="ui container">
+        <div>
+          <Header />
           {/* exact property makes sure that home page component will show only exactly on the home page url */}
-
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/:id" component={StreamShow} />
-          <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/edit/:id" component={StreamEdit} />
-          <Route path="/streams/delete/:id" component={StreamDelete} />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" component={StreamCreate} />
+            <Route path="/streams/edit/:id" component={StreamEdit} />
+            <Route path="/streams/delete/:id" component={StreamDelete} />
+            <Route path="/streams/:id" component={StreamShow} />
+          </Switch>
         </div>
       </Router>
     </div>
