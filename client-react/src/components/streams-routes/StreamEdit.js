838d6642139d7af9bbbeb7fae15f988b51aa7b09
L3n1ad <danieltoth0910@gmail.com>
2020-07-10 11:02:56 +0100

    refactor StreamEdit to use StreamFrom

--- client-react/src/components/streams-routes/StreamEdit.js
+++ client-react/src/components/streams-routes/StreamEdit.js
@@ -1,16 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions/index";
+import { fetchStream, editStream } from "../../actions/index";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
+
+  onSubmit = (formValues) => {
+    console.log(formValues);
+  };
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    return <div>{this.props.stream.title}</div>;
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        {/* initialValues is a special thing for redux Form which will use the pass down values to fill up the fields in the form, naming is important it has to be the same as the naming in the form */}
+        <StreamForm
+          // initialValues={{ title: "EDIT ME", description: "CHANGE ME TOO" }}
+          initialValues={this.props.stream}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
   }
 }
 
@@ -20,4 +36,6 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
